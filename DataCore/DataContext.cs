023846f19536dd2c2162f1d0461b5d2db3a2fdfa
Steve Graddy <orgbrat@orgbrat.com>
2024-07-24 14:10:57 -0500

    Initial Work

---
+++ DataCore/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataCore
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public virtual DbSet<DataXml> XmlData { get; set; }
+
+    }
+}
---
+++ DataCore/DataXml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCore
+{
+    public class DataXml
+    {
+        public int DataId { get; set; }
+    }
+}
